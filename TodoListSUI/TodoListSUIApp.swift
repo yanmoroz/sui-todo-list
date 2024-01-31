6fc30e156db651846c0fe07c7c613d779c3bedd7
Yan Moroz <mail@yanmoroz.ru>
2024-02-01 00:31:45 +0300

    Initial Commit

---
+++ TodoListSUI/TodoListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSUIApp.swift
+//  TodoListSUI
+//
+//  Created by Yan Moroz on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
